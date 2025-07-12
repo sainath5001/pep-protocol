02b7d2c59dfdeb96e6ec58798e96c0dd6224ca60
Sainath More <sainathmore501@gmail.com>
2025-07-12 07:45:02 +0000

    Working on src folder in that DecentralizedStableCoin.sol file or created DSCEngine.sol file in src or strted working on that

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -23,12 +23,11 @@
 // private
 // view & pure functions
 
-
 pragma solidity 0.8.20;
 
 /*
  * @title DecentralizedStableCoin
- * @author Patrick Collins
+ * @author Sainath More
  * Collateral: Exogenous
  * Minting (Stability Mechanism): Decentralized (Algorithmic)
  * Value (Relative Stability): Anchored (Pegged to USD)
@@ -39,11 +38,36 @@ DSCEngine smart contract.
  */
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DecentralizedStableCoin is ERC20Burnable{
-    constructor() ERC20("Decentralized Stable Coin", "DSC") {
-        
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin_MustBeMoreThanZero();
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+    error DecentralizedStableCoin_NotZeroAddress();
+
+    constructor() ERC20("Decentralized Stable Coin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        }
+
+        // Only the owner can burn DSC
+        super.burn(_amount);
     }
 
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin_NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
-
