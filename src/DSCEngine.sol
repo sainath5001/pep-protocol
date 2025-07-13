7a42bf10bffa3f94279fb7f509fa900ad6592b9f
Sainath More <sainathmore501@gmail.com>
2025-07-13 14:37:08 +0000

    Working on src folder in that DecentralizedStableCoin.sol file or created DSCEngine.sol file in src or strted working on that

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -25,6 +25,7 @@
 
 pragma solidity ^0.8.20;
 
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /*
